f31778f930ff5ea2ea03217a4d4780eff7dbc61e
Michael Davis <michaeldavis@mail.dk>
2021-10-13 23:40:16 +0200

    execToDo

---
+++ ExerToDo.R
@@ -0,0 +1,51 @@
+################# #Exercises to do ######################
+############### Exercises ######################## 
+pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes,
+               ggvis,httr, lubridate, plotly, rio, rmarkdown, shiny,
+               stringr, tidyr, caret, lars, tidyverse, psych, dygraphs,
+               vioplot, gapminder, nycflights13, gapminder, Lahman, ISLR2,
+               hms, feather, haven, readxl, DBI, jsonlite, xml2, lvplot 
+)
+
+
+
+
+### 
+library(dplyr)
+library(nycflights13)
+library(tidyverse)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# CLEAN UP #################################################
+
+# Clear environment
+rm(list = ls()) 
+
+# Clear packages
+detach("package:datasets", unload = TRUE)  # For base
+
+# Clear plots
+dev.off()  # But only if there IS a plot
+
+# Clear console
+cat("\014")  # ctrl+L
+
+# Clear mind :)
